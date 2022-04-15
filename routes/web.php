93cf61deec556b83607043a6bb997f67eafcc831
tpipernob <tiagopiperno@gmail.com>
2022-04-15 11:42:07 -0300

    detail product

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/product', [ProductController::class, 'show']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/product/{product}', [ProductController::class, 'show'])->name('product');
 
 //Admin
-Route::get('/admin/products', [AdminProductController::class, 'index']);
-Route::get('/admin/edit', [AdminProductController::class, 'edit']);
+Route::get('/admin/products', [AdminProductController::class, 'index'])->name('admin.products');
+Route::get('/admin/edit', [AdminProductController::class, 'edit'])->name('admin.products.edit');
