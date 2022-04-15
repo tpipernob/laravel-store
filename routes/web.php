6b2537a4ae9b37085ca3417429175117cc72c762
tpipernob <tiagopiperno@gmail.com>
2022-04-15 10:58:36 -0300

    create database and product seeder

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/product', [ProductController::class, 'show']);
 
