d3b9f17b8fa51a36b9b3aaf00ccdf43b3e1d9288
tpipernob <tiagopiperno@gmail.com>
2022-04-15 16:36:03 -0300

    add slug

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/product/{product}', [ProductController::class, 'show'])->name('product');
+Route::get('/product/{product:slug}', [ProductController::class, 'show'])->name('product');
 
 //Admin
 Route::get('/admin/products', [AdminProductController::class, 'index'])->name('admin.products');
